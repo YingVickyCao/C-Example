9895583235b79425306a0aa50ea21f1d2d3de2cb
Vicky <ying.vicky.cao@outlook.com>
2022-10-31 21:08:27 +0800

    code

---
+++ Lean-C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Lean-C
+//
+//  Created by hades on 2022/10/31.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
