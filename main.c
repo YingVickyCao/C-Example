8a72ca4c880a8d93b9756545c81fc3055a8d1b99
Vicky <ying.vicky.cao@outlook.com>
2022-10-31 22:56:56 +0800

    first main

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
-#include <stdio.h>
+// 1 点击运行按钮，XCode自动帮我们编译-链接-执行
+// 2 实时检测语法错误
+// 3 关键字自动着色，不同颜色表示不同的含义。
+// 4 代码自动缩进
+// 5 代码自动提示
 
-int main(){
-  printf("Hello World\n");
-  return 0;
+// XCode编译之后，生成的可执行文件在哪里？Products -> 右键 -> Show in Finder
+#include "stdio.h"
+
+int main(int argc, const char* argv[]){
+    printf("Hello World\n");
+    return 0;
 }
