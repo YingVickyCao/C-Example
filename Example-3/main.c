8df6c22cdb74e968daee67ff1835f31b6a870862
Vicky <ying.vicky.cao@outlook.com>
2022-11-01 22:47:08 +0800

    C

---
+++ Example-3/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Example-3
+//
+//  Created by hades on 2022/11/1.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Example 3");
+    return 0;
+}
---
+++ Example2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Example2
+//
+//  Created by hades on 2022/11/1.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Welcome\n");
+    return 0;
+}
