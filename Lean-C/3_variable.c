0dc8fc0cd011a427ac8f6ada33315bef56059e92
Vicky <ying.vicky.cao@outlook.com>
2022-11-08 21:00:00 +0800

    C 变量

--- Lean-C/3_variable.c
+++ Lean-C/3_variable.c
@@ -6,15 +6,19 @@ void set_double_value();
 void set_char_value();
 void use_variable();
 void variable_tip();
+void use_printf();
+void prinf_int();
+void prinf_double();
 
 int main(int argc, const char* argv[]){
-    printf("Hello World\n");
+//    printf("Hello World\n");
 //    set_int_value();
 //    set_float_value();
 //    set_double_value();
 //    set_char_value();
 //    use_variable();
-    variable_tip();
+//    variable_tip();
+    use_printf();
     return 0;
 }
 
@@ -220,3 +224,62 @@ void variable_tip(){
     int n1 = num;
     printf("%d\,%d\n",num,n1); // 30,30
 }
+
+/**
+ prinft : 向控制台输出信息
+
+ int
+ (1)%md  m 是整数，指定输出变量时占多少位.
+ e.g., %5d,
+ 如果变量小于5位，不足用空格补齐。
+ 如果变量>=5位，实际有多少位就显示多少位
+
+ e.g., %05d, 如果变量小于5位，不足用0补齐
+ 
+ m为正数，前面补齐，
+ m为负数，后面补齐
+
+ (2)double  %.nlf 指定输出小数点后的位数
+ (3)float  %.nf 指定输出小数点后的位数
+ */
+void use_printf(){
+//    prinf_int();
+    
+    prinf_double();
+}
+
+
+void prinf_int()
+{ // Int
+    int num = 10;
+    //10
+    printf("%d\n",num);
+    //   10
+    printf("%5d\n",num); // %md
+    int x = 123456789;
+    // 123456789
+    printf("%5d\n",x); // %md
+    //10   XXX
+    printf("%-5dXXX\n",num); // %md
+    
+    //00010
+    printf("%05d\n",num);
+    
+    
+}
+
+void prinf_double(){
+    // Double
+    double d = 10.1234567890;
+    // 10.123457
+    printf("%lf\n",d); // Default 输出 6 位。 7 四舍五入后进位1
+    
+    // Double
+    double d2 = 10.123456123;
+    // 10.123456
+    printf("%lf\n",d2); // Default 输出 6 位。 1 四舍五入是0，不进位，
+    
+    // 10.12
+    printf("%.2lf\n",d); // 指定输出小数点后的2位
+}
+
