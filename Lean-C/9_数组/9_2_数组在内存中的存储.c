346f501a4152a4f0a8a837d1777c0146d419f5fa
Vicky <ying.vicky.cao@outlook.com>
2022-12-09 22:22:54 +0800

    数组的长度

--- Lean-C/9_数组/9_2_数组在内存中的存储.c
+++ Lean-C/9_数组/9_2_数组在内存中的存储.c
@@ -1,7 +1,8 @@
 //  数组在内存中的存储
 #include <stdio.h>
 
-int main(int argc, const char *argv[]) {
+//int main(int argc, const char *argv[]) {
+int main_9_2(int argc, const char *argv[]) {
     int array[3];
     array[0] = 1;
     array[1] = 2;
@@ -13,11 +14,11 @@ int main(int argc, const char *argv[]) {
     
     // 3、数组的长度计算
     int length = sizeof(array);
-    int element_length = sizeof(int);
+//    int element_length = sizeof(4); // not good
+    int element_length = sizeof(int); // good
+//    int element_length = sizeof(array[0]);// good
     int size = length/element_length;
     printf("total length = %d, elemet length = %d, size = %d\n",length,element_length,size);// 12, 4, 3
-    
-    
     return 0;
 }
 
@@ -50,6 +51,9 @@ int main(int argc, const char *argv[]) {
     1）、数组的每一个元素的类型相同，所以数组的每一个元素占用的空间是一样的。
     2）、使用sizeof 计算数组总共占用的字节数。
     3）、得到数组占用的总字节数，那么用总字节数/每个字节占用的字节数，那么得到数组的长度。
- 
+        sizeof(数组名)/sizeof（每1个元素的字节数）// not good
+    4)、不建议将字节数写死。元素占用的字节数建议用sizeof计算出来，因为不同系统、不同的编译器，相同的变量可能占用的字节不一样。
+        sizeof(数组名)/sizeof（元素类型）// good
+        sizeof(数组名)/sizeof（数组名[0]）// good
  
  */
---
+++ Lean-C/9_数组/9_3_数组的应用.c
@@ -0,0 +1,8 @@
+// 数组的应用.c
+
+#include <stdio.h>
+
+int main(int argc, const char *argv[]) {
+//int main_9_3(int argc, const char *argv[]) {
+    return 0;
+}
